91e1f621c134a2eff80eca5dd29e710284bdbeca
Isam Ali <119074096+Isam-Saed@users.noreply.github.com>
2022-12-24 19:36:06 +0300

    Add files via upload

---
+++ master.js
@@ -0,0 +1,110 @@
+let landingPage = document.querySelector('.landing-page');
+let Setiing1 =document.querySelector('.setting-box');
+let SettingIcon = document.querySelector('.setting-box .fa-solid');
+let imgArray =['01.avif','02.avif','03.avif','04.avif','06.avif','16.jpg'];
+const selectColor = document.querySelectorAll('.colors-list li');
+let main_color = localStorage.getItem("color_option");
+let ourGallary =document.querySelectorAll('.gallary img');
+
+
+landingPage.style.backgroundImage = 'url("13.jpg")';
+
+document.querySelector('.toggle-setting .fa-solid').onclick = function(){
+    this.classList.toggle("fa-spin");
+    document.querySelector('.setting-box').classList.toggle("open");
+    }
+
+/*etInterval( ()=>{
+    let randomNumber =Math.floor(Math.random() * imgArray.length);
+    if(randomNumber >0){ 
+    landingPage.style.backgroundImage = `url("0${randomNumber}.avif")`;}
+    
+    //landingPage.style.backgroundImage = 'url("../image/' +  imgArray[randomNumber]+")';
+},1800)*/
+
+
+if(main_color !== null){
+    document.documentElement.style.setProperty('--main-color',main_color);
+}
+
+selectColor.forEach(li =>{
+    li.addEventListener('click',(e)=>{
+        document.documentElement.style.setProperty('--main-color',e.target.dataset.color);
+        localStorage.setItem('color_option',e.target.dataset.color)
+    })
+})
+
+
+let ourSkills = document.querySelector('.skills');
+window.onscroll=function(){
+    let skillsOffSetTop = ourSkills.offsetTop;
+
+    let skillsOuterHeight = ourSkills.offsetHeight;
+   
+    let windowHeight = this.innerHeight;
+
+    let windowScrollTop = this.pageYOffset;
+  
+    if(skillsOffSetTop >(skillsOffSetTop + skillsOuterHeight - windowHeight)){ 
+    let allSkills = document.querySelectorAll('.skill-box .skill-progress span');
+allSkills.forEach(skills =>{
+    skills.style.width=skills.dataset.progress;
+})
+}
+}
+
+
+ /*async function get(){
+    const response  = await fetch("https://www.breakingbadapi.com/api/characters/1");
+    const data = await response.json();
+    console.log(data)
+}
+get();*/
+
+ourGallary.forEach(img =>{
+
+    img.addEventListener('click',(e) =>{
+        let overLay = document.createElement('div');
+        overLay.className = 'popup-overlay';
+
+        document.body.appendChild(overLay);
+
+        let popupBox = document.createElement('div');
+        popupBox.className='popup-box';
+
+        let popupImage = document.createElement('img');
+        popupImage.src = img.src;
+        popupBox.appendChild(popupImage);
+        document.body.appendChild(popupBox)
+
+    })
+})
+
+
+/*const allLinks = document.querySelectorAll('.links a');
+allLinks.forEach(link =>{
+link.addEventListener('click',(e)=>{
+    e.preventDefault();
+document.querySelector(e.target.dataset.section).scrollIntoView({
+    behavior: 'smooth'
+})
+})
+})*/
+
+let hiddinhH1 = document.querySelector('.introduction-text');
+
+let toggleBtn = document.querySelector('.toggle-menu');
+let links = document.querySelector('.links');
+
+toggleBtn.onclick=function(){
+   hiddinhH1.classList.toggle('hidden');
+    this.classList.toggle("menu-active");
+    links.classList.toggle("open");
+    hiddinhH1.classList.toggle('.visable');
+  
+}
+
+
+
+
+
